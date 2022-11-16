5732cbffe6bdf3d6c2108baadd4ee15437e1d4ed
Gary Miguel <garymm@garymm.org>
2022-11-16 15:14:50 -0800

    format with black

--- lab/source_dir/setup.py
+++ lab/source_dir/setup.py
@@ -3,9 +3,11 @@
 
 from setuptools import setup, find_packages
 
-setup(name='sagemaker-example',
-      version='1.0',
-      description='SageMaker MLFlow Example.',
-      author='sofian',
-      author_email='hamitis@amazon.com',
-      packages=find_packages(exclude=('tests', 'docs')))
+setup(
+    name="sagemaker-example",
+    version="1.0",
+    description="SageMaker MLFlow Example.",
+    author="sofian",
+    author_email="hamitis@amazon.com",
+    packages=find_packages(exclude=("tests", "docs")),
+)
