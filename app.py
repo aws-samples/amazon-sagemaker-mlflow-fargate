2efea0bd13734e16befdb8c7c57c6bbbb61c281e
Harish kumar <36706840+Harishpandu43@users.noreply.github.com>
2024-06-14 16:00:07 +0530

    Update DB instance family as per the latest version of RDS
    Update DB instance family as per the latest version of RDS

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ class MLflowStack(Stack):
                 version=rds.MysqlEngineVersion.VER_8_0_34
             ),
             instance_type=ec2.InstanceType.of(
-                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL
+                ec2.InstanceClass.M5, ec2.InstanceSize.LARGE
             ),
             vpc=vpc,
             security_groups=[sg_rds],
